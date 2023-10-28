a2e64a46aa5d36b8d61e7392414fff9f48c7857c
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-10-28 16:02:34 +0000

    sm8350-common: Build OMX sevice after stop shipping 32-bit Zygote
    [1] - https://gerrit.aospa.co/c/AOSPA/android_vendor_aospa/+/32966
    [2] - https://github.com/AOSPA/android_build/commit/628d5665b128f866462a5a3a86013d4f1ffa81d4
    Change-Id: I3ecec94eb8fc62a2fdc076840fa8aee98a83456d
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -317,6 +317,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
     libcodec2_hidl@1.0.vendor \
     libcodec2_soft_common.vendor \
     libmm-omxcore \
